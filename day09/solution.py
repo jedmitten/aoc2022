721aee0ebef610e11cebffb36eddf7ff1be5d30f
Jed Mitten <jedmitten@gmail.com>
2022-12-10 00:32:21 -0800

    Day09 Complete (#9)
    * Day08 begun. Initial Map development finished
    
    * stub work
    
    * add url
    
    * passed day08 part 1 tests
    
    * day08 solved
    
    * day09 passing tests for part 1
    
    * day09 complete

---
+++ day09/solution.py
@@ -0,0 +1,145 @@
+# AOC 2022 Day 9 https://adventofcode.com/2022/day/0
+import math
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+
+DAY = 9
+DAY_STR = f"{DAY:02d}"
+
+
+class Direction:
+    Left = -1
+    Right = 1
+    Up = 1
+    Down = -1
+
+
+def read_input(filepath: str) -> List[str]:
+    """Read input file and return a list of something"""
+    filepath = Path(filepath).expanduser().absolute()  # type: Path
+    if not filepath.exists:
+        raise ValueError(
+            f"The path {filepath.expanduser().absolute()} could not be found"
+        )
+    with open(filepath) as f:
+        lines = f.readlines()
+    return [_l.strip() for _l in lines if _l]
+
+
+def parse(lines: List[str]) -> List[Tuple[int, int, int]]:
+    """Given the input, parse into instructions
+    R 4 = Right 4, so use add 4 to the head x-axis
+    That means return (1, 0, 4) for x-axis multiplier, y-axis multiplier, 4 units
+    """
+    for line in lines:
+        x = y = 0
+        direction, count = line.split(" ")
+        if direction == "R":
+            x = Direction.Right
+        elif direction == "L":
+            x = Direction.Left
+        elif direction == "U":
+            y = Direction.Up
+        else:
+            y = Direction.Down
+        yield (x, y, int(count))
+
+
+def distance(p1: Tuple[int, int], p2: Tuple[int, int]) -> int:
+    """Distance between 2 points on a plane"""
+    x1 = p1[0]
+    x2 = p2[0]
+    y1 = p1[1]
+    y2 = p2[1]
+
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def move_tail_toward_head(head: Tuple[int, int], tail: Tuple[int, int]) -> Tuple[int, int]:
+    """
+    If p1 is left of p2, return (-1, 0)
+       p1 is left and above p2, return (-1, 1)
+       ...
+    """
+    x = y = 0
+    if distance(head, tail) > math.sqrt(2):
+        if head[0] < tail[0]:  #left
+            x = Direction.Left
+        if head[0] > tail[0]:  # right
+            x = Direction.Right
+        if head[1] < tail[1]:
+            y = Direction.Down
+        if head[1] > tail[1]:
+            y = Direction.Up
+    return (tail[0] + x, tail[1] + y)
+
+
+def move_head(
+    from_coord: Tuple[int, int], coord_update: Tuple[int, int]
+) -> Tuple[int, int]:
+    """Move a point in the direction(s) indicated by coord_update"""
+    return (
+        from_coord[0] + (1 * coord_update[0]),
+        from_coord[1] + (1 * coord_update[1]),
+    )
+    
+    
+def visualize(head, tail, map):
+    map[head] = "H"
+    if not isinstance(tail, list):
+        tail = [tail]
+    for i in range(tail):
+        map(tail[i]) == i
+    print(map)
+    
+    
+def init_map(x, y) -> Dict[int, Dict[int, str]]:
+    map = dict()
+    for i in range(y):
+        for j in range(x):
+            map.update({j: {i: "."}})
+    map[0][0] = "s"
+    return map
+
+
+def print_map(map: Dict[int, Dict[int, str]]) -> None:
+    max_x = max(list(map.keys()))
+    for x, y in map.items():
+        print(map[x][y])
+        if y % max_x == 0:
+            print("")        
+
+
+def solve_pt1(lines: List[str]) -> int:
+    h = t = (0, 0)
+    tail_spaces = set(list())
+    for x, y, count in parse(lines):
+        update = (x, y)
+        # move in the direction, count spaces, one at a time
+        for _ in range(count):
+            tail_spaces.add(t)
+            h = move_head(h, update)
+            t = move_tail_toward_head(h, t)
+    tail_spaces.add(t)
+    return len(tail_spaces)
+
+
+def solve_pt2(lines: List[str]) -> int:
+    TAIL_LENGTH = 9
+    LAST_KNOT = TAIL_LENGTH - 1
+    h = (0, 0)
+    t = [(0, 0) for i in range(TAIL_LENGTH)]
+
+    tail_spaces = set(list())
+    for x, y, count in parse(lines):
+        update = (x, y)
+        # move in the direction, count spaces, one at a time
+        for _ in range(count):
+            tail_spaces.add(t[LAST_KNOT])
+            h = move_head(h, update)
+            t[0] = move_tail_toward_head(h, t[0])
+            for i in range(1, TAIL_LENGTH):
+                t[i] = move_tail_toward_head(t[i-1], t[i])
+    tail_spaces.add(t[LAST_KNOT])
+    return len(tail_spaces)
---
+++ day09/test_soln.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+from typing import List
+from pytest import fixture
+
+from . import solution as soln
+
+
+P1 = (0, 0)
+P2 = (1, 0)
+P3 = (1, 1)
+
+
+@fixture
+def sample_input() -> List[int]:
+    return soln.read_input(f"~/GitRepos/aoc2022/day{soln.DAY_STR}/test_input.txt")
+
+@fixture
+def sample_input2() -> List[int]:
+    return soln.read_input(f"~/GitRepos/aoc2022/day{soln.DAY_STR}/test_input_2.txt")
+
+
+def test_distance():
+    assert soln.distance(P1, P2) == 1
+    assert soln.distance(P2, P3) == 1
+    assert soln.distance(P1, P3) > 1
+
+
+def test_move_head():
+    assert soln.move_head(P1, (1, 0)) == (1, 0)
+    assert soln.move_head(P2, (1, 1)) == (2, 1)
+
+
+def test_move_tail_toward_head():
+    head = (3, 3)
+    tail = (2, 3)
+    assert soln.move_tail_toward_head(head, tail) == tail
+    head = (3, 4)
+    assert soln.move_tail_toward_head(head, tail) == tail
+    head = (3, 5)
+    assert soln.move_tail_toward_head(head, tail) == (3, 4)
+
+
+
+def test_parse(sample_input):
+    moves = list(soln.parse(sample_input))
+    for x, y, count in moves:
+        assert isinstance(x, int)
+        assert isinstance(y, int)
+        assert isinstance(count, int)
+    assert moves[0] == (1, 0, 4)
+    
+
+def test_solve_pt1(sample_input):
+    assert soln.solve_pt1(sample_input) == 13
+    
+
+def test_solve_pt2(sample_input):
+    assert soln.solve_pt2(sample_input) == 1
+    
+
+def test_solve_pt2_2(sample_input2):
+    assert soln.solve_pt2(sample_input2) == 36
