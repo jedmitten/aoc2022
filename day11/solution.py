7530a41d466cf94c51f803736e8e9bb10062d976
Jed Mitten <jedmitten+githubconfig@gmail.com>
2022-12-11 22:39:14 -0800

    major pieces of monkey behavior modeled

--- day11/solution.py
+++ day11/solution.py
@@ -1,6 +1,7 @@
 # AOC 2022 Day 11 https://adventofcode.com/2022/day/11
+import math
 from pathlib import Path
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 
 
 DAY = 11
@@ -9,24 +10,42 @@ DAY_STR = f"{DAY:02d}"
 
 class Monkey:
     def __init__(self):
-        self.starting_items = None
+        self.items = None
         self.operation = None
         self.test = None
         self.throw_true = None
         self.throw_false = None
 
-    def inspect(self, item):
-        raise NotImplementedError()
+    def inspect_next(self) -> int:
+        # get the next item
+        worry = self.items.pop(0)
+        return self.perform_operation(worry)
 
-    def test_worry(self, worry):
-        raise NotImplementedError()
+    def perform_operation(self, item) -> int:
+        """Use the self.operation string to eval math"""
+        operation = self.operation.replace("old", "item")
+        new_worry = eval(operation)
+        return new_worry
 
-    def throw_to(self, other_monkey, val):
-        raise NotImplementedError()
+    def test_worry(self, worry) -> bool:
+        # check if number is divisible by self.test
+        return not worry % self.test
+
+    def get_monkey_and_worry(self) -> Tuple[int, int]:
+        # 1. Inspect
+        worry = self.inspect_next()
+        # 2. I settle down
+        # Monkey gets bored with item. Worry level is divided by 3 to 500.
+        worry = math.floor(worry / 3)
+        # 3. Perform operation
+        test_true = self.test_worry(worry)
+        # 4. Perform throw based on test_true
+        next_monkey_idx = self.throw_true if test_true else self.throw_false
+        return worry, next_monkey_idx
 
 
 def read_input(filepath: str) -> List[str]:
-    """Read input file and return a list of something"""
+    """Read input file and return a list of string inputs"""
     filepath = Path(filepath).expanduser().absolute()  # type: Path
     if not filepath.exists:
         raise ValueError(
@@ -44,7 +63,7 @@ def parse(lines: List[str]) -> List[Monkey]:
     while i < len(lines):
         line = lines[i]
         if line.startswith("Monkey"):
-            monkey_inits.append(lines[i:i+fwd])
+            monkey_inits.append(lines[i : i + fwd])
             i += fwd
         else:
             i += 1
@@ -53,9 +72,10 @@ def parse(lines: List[str]) -> List[Monkey]:
     for init in monkey_inits:
         m = Monkey()
         _, items = init[1].split(":")
-        m.starting_items = [int(i.strip()) for i in items.split(",")]
+        m.items = [int(i.strip()) for i in items.split(",")]
         _, op = init[2].split(":")
-        m.operation = op.strip()
+        op = op.split("=")[1].strip()
+        m.operation = op
         _, div_init = init[3].split(":")
         m.test = int(div_init.replace(" divisible by ", "").strip())
         _, if_true = init[4].split(":")
@@ -68,7 +88,23 @@ def parse(lines: List[str]) -> List[Monkey]:
 
 
 def solve_pt1(lines: List[str]) -> int:
-    return ""
+    monkeys = parse(lines)
+    # The monkeys take turns inspecting and throwing items
+    i = 0
+    while True:
+        worry, target_monkey_idx = monkeys[0].get_monkey_and_worry()
+        # throw the item
+        target_monkey = monkeys[target_monkey_idx]
+        target_monkey.items.append(worry)
+        # go to next monkey
+        i = (i + 1) % len(monkeys)
+
+    """
+    After each monkey inspects an item but before it tests your worry level,
+ x  your relief that the monkey's inspection didn't damage the item causes
+    your worry level to be divided by three and rounded down to the nearest
+    integer.
+    """
 
 
 def solve_pt2(lines: List[str]) -> int:
--- day11/test_soln.py
+++ day11/test_soln.py
@@ -11,7 +11,21 @@ def sample_input() -> List[str]:
 
 def test_parse(sample_input):
     monkeys = soln.parse(sample_input)
-    pass
+    m = monkeys[0]
+    assert m.test == 23
+    assert m.operation == "old * 19"
+    assert m.items == [79, 98]
+    assert m.throw_true == 2
+    assert m.throw_false == 3
+
+
+def test_monkey(sample_input):
+    monkeys = soln.parse(sample_input)
+    m = monkeys[0]
+    expected = (500, 3)
+    found = m.get_monkey_and_worry()
+    assert expected == found
+
 
 def test_solve_pt1(sample_input):
     assert soln.solve_pt1(sample_input) == 13140
